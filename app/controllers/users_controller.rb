de4d4bd6dfcf2c3c4d30b40430d9e7be4728a739
qasimali-tx <qasimali@techcreatix.com>
2019-07-26 14:32:00 +0000

    Add user Model,Controller also user.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,17 @@ class UsersController < ApplicationController
     @users =User.all
   end
 
+  def destroy
+
+    @user = User.find(params[:id])
+      if current_user.id == @user.id
+        flash.now[:danger] = 'Invalid action perform'
+        redirect_to users_path
+
+      else
+        @user.destroy
+        redirect_to users_path
+      end
+  end
+
 end
